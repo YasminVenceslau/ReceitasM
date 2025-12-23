cc7384329109e828beb0117437667e6612af32a5
Yasmin Venceslau <yasmin.venceslau@hotmail.com>
2025-12-22 23:49:07 -0300

    add

---
+++ src/data/receitas.ts
@@ -0,0 +1,367 @@
+
+export const receitas = [
+    {
+    "id": 1,
+    "nome": "Bolo de Frutas Vermelhas",
+    "categoria": "Sobremesa",
+    "tags": [
+        "#bolo",
+        "#doce",
+        "#frutas",
+        "#sobremesa",
+        "#confeitaria"
+    ],
+    "forma": "Bolo inglês 30x10cm",
+    "rendimento": "1 bolo (aprox. 10 a 12 fatias)",
+    "tempo_preparo": {
+        "preparo": "30 minutos",
+        "forno": "40 a 45 minutos",
+        "total": "1h10min a 1h15min"
+    },
+    "ingredientes": {
+        "massa": [
+        "4 ovos",
+        "1 xícara de açúcar (200g)",
+        "1 colher de chá de essência de baunilha",
+        "60ml de óleo",
+        "70ml de leite",
+        "1 xícara de farinha de trigo (160g)",
+        "1 colher de sopa de fermento em pó"
+        ],
+        "recheio": [
+        "1 lata de leite condensado",
+        "2 caixinhas de creme de leite",
+        "1 gema",
+        "1/2 xícara de leite em pó",
+        "150g de cream cheese"
+        ],
+        "calda": [
+        "200ml de água filtrada",
+        "1 colher de sopa de leite em pó",
+        "ou água com leite condensado"
+        ],
+        "reducao_frutas_vermelhas": [
+        "450g de frutas vermelhas (amora, framboesa, mirtilo e morango)",
+        "3 colheres de sopa de açúcar",
+        "3 colheres de sopa de água (somente se as frutas forem frescas)"
+        ]
+    },
+    "modo_preparo": {
+        "massa": [
+        "Preaqueça o forno a 180°C.",
+        "Em uma tigela, bata os ovos com o açúcar até obter um creme claro.",
+        "Adicione a baunilha, o óleo e o leite, misturando bem.",
+        "Incorpore a farinha de trigo aos poucos.",
+        "Finalize adicionando o fermento e misture delicadamente.",
+        "Despeje a massa em forma untada e enfarinhada.",
+        "Asse por 40 a 45 minutos ou até passar no teste do palito."
+        ],
+        "recheio": [
+        "Em uma panela, misture o leite condensado, o creme de leite, a gema e o leite em pó.",
+        "Leve ao fogo médio, mexendo sempre até engrossar.",
+        "Desligue o fogo e acrescente o cream cheese, misturando até ficar homogêneo.",
+        "Reserve até esfriar."
+        ],
+        "reducao_frutas_vermelhas": [
+        "Coloque as frutas vermelhas e o açúcar em uma panela.",
+        "Se as frutas forem frescas, adicione a água.",
+        "Leve ao fogo médio até formar uma calda levemente espessa.",
+        "Reserve para esfriar."
+        ],
+        "montagem": [
+        "Desenforme o bolo já frio e corte ao meio.",
+        "Regue com a calda escolhida.",
+        "Recheie com o creme e adicione a redução de frutas vermelhas.",
+        "Cubra com a outra parte do bolo ou finalize como preferir."
+        ]
+    }
+},
+
+{
+    "id": 2,
+    "nome": "Sorvete de Ameixa",
+    "categoria": "Sobremesa",
+    "tags": [
+        "#doce",
+        "#fruta",
+        "#sorvete",
+        "#gelado",
+    ],
+    "forma": "Pote ou forma para freezer",
+    "rendimento": "aprox. 1,5 litro",
+    "tempo_preparo": {
+        "preparo": "20 minutos",
+        "forno": "não se aplica",
+        "total": "4 a 6 horas (incluindo congelamento)"
+    },
+    "ingredientes": {
+        "massa": [
+            "1 litro de leite",
+            "2 gemas",
+            " 1 colher de sobremesa de manteiga",
+            "2 colheres de sopa de maizena",
+            "1 colher de licor de cacau",
+            "Açúcar a gosto",
+            "100g de ameixa",
+            "1 caixinha de creme de leite",
+            "1 colher de chá de essência de baunilha (opcional)"
+        ],
+    },
+    "modo_preparo": {
+        "massa": [
+            "Fazer um doce de ameixa",
+        ],
+        "como": [
+            " Passar no liquidificador, misturar ao creme e colocar as 2 claras já em neve. Leve ao congelador ( bater  duas Vezes",
+        ],
+        
+    }
+},
+{
+    "id": 3,
+    "nome": "Lagarto Marinado",
+    "categoria": "Carnes",
+    "tags": [
+        "#salgado",
+        "#carne",
+        "#almoco",
+        "#jantar",
+        "#lagarto",
+        "#assado",
+    ],
+    "Panela": " sim ",
+    "rendimento": "10",
+    "tempo_preparo": {
+        "preparo": "15 a 20 minutos",
+        "marinado":'8 a 12 horas ' ,
+        "Cocção": " 1h30 a 2h",
+        "total": " 10 a 14 horas"
+    },
+    "ingredientes": {
+        "carne": [
+            "2 quilos de largato em bife",
+            "2 cebolas",
+            "2 pimentões",
+            " 1 limão",
+            "pimenta do reino",
+            "sal",
+            "alho",
+            "louro",
+            "salsa",
+            "cebola verde",
+            " oregano",
+        ],
+        "molho": [
+            "1 copo de vinagre",
+            "1 copo de vinho branco ou madeira",
+            "pimenta do reino",
+            "sal",
+            "oregano",
+            "4 cebolas picada",
+            "tomate",
+            "pimentão verde",
+            "salsa", 
+            "pimenta calbreza",
+        ],
+    },
+    "modo_preparo": {
+        "carne": [
+            'Coloque a carne para cozinhar com todos os temperos, arrumando cada bife bem comprimido ao outro. Tampe e deixe cozinhar lentamente e se possível pingue água.'
+        ],
+        "molho": [
+            'Quando a carne tiver xozidam retire da panela e coloque neste molho.  '
+        ],
+        "montagem": [
+            'Tanto ser parasevir frio como quente.'
+        ]
+    }
+},
+{
+    "id": 4,
+    "nome": "Carne a Ingleza",
+    "categoria": "Carnes",
+    "tags": [
+        "#salgado",
+        "#carne",
+        "#almoco",
+        "#jantar",
+        "#lagarto",
+        "#assado",],
+    "forma": "",
+    "rendimento": "15 pessoas",
+    "tempo_preparo": {
+        "preparo": " 20 a 30 minutos",
+        "Cocção": " 2 a 2h30",
+        "Resfriamento (para fatiar)": " 2 a 4 horas",
+        "total": "4h30 a 7h"
+    },
+    "ingredientes": {
+        "carne": [
+            "3 quilos de lagarto",
+            " 1/2 quilo de cebolas pequenas",
+            "250g de cenoura cozida",
+            "1 envelopre de bacon",
+            "3 ovos",
+            "Farinha de mandioca",
+            "Sal",
+            'farora com manteiga e duas cebolas picadas'
+        ],
+    },
+    "modo_preparo": {
+        "carne": [
+            'Corte o lagarto ao meio. Tempere com sal. A parte faça uma farora com manteiga e duas cebolas picadas, abra a carne e espalhe o baicon, ponha a farofa e feche amarrando apertado. Coloque na panela de pressao até ficar macia.'
+        ],
+        "montagem": [
+            'Sirva com as cenoras e cebolas cozidas.'
+        ]
+    }
+},
+{
+    "id": 5,
+    "nome": "Torta mexida",
+    "categoria": "Salgado",
+    "tags": [
+        "#torta",
+        "#salgado",
+        "#camarão",
+        "#forno",
+        "#lanche"
+    ],
+    "forma": "Forma média retangular ou redonda untada",
+    "rendimento": "8 a 12 fatias",
+    "tempo_preparo": {
+    "preparo": "15 a 20 minutos",
+    "forno": "40 a 50 minutos",
+    "total": "55 a 1h10"
+    },
+    "ingredientes": {
+        "massa": [
+            "1 lata de ervilha",
+            "1 lata de palmito",
+            "1/2 quilo de camarão (pescal)",
+            "1 tomate picado",
+            "1 cebola batida",
+            "1 punhado de cheiro verde",
+            "alho a vontade",
+            "2 ovos cruz inteiros",
+            "1 xícara  de leite",
+            "1 xícara de queijo ralado",
+            '1 xícara de óleo',
+            "1 colher de sobremesa de fermento em pó (Pó Royal)"
+        ],
+        
+    },
+    "modo_preparo": {
+        "massa": [
+            'Misture tudo junto, por 1 colher de sobremesa de pó royal. Untar uma forma e levar ao forno já quente'
+        ],
+        
+    }
+},
+{
+    "id": 6,
+    "nome": "Lombo marinado",
+    "categoria": "Carnes",
+    "tags": [
+        "#salgado",
+        "#carne",
+        "#almoco",
+        "#jantar",
+        "#lombo",
+        "#assado",
+    ],
+    "forma": "Assadeira grande",
+    "rendimento": "6 a 8 porções",
+    "tempo_preparo": {
+        "preparo": "20 minutos",
+        "coccao": "1h30",
+        "total": "aprox. 14 horas (incluindo marinada)"
+    },
+    "ingredientes": {
+        "marinada": [
+            "1,5 kg de lombo",
+            "3 xícaras de vinho branco seco",
+            "1/2 xícara de suco de limão",
+            "2 folhas de louro",
+            "3 ramos de salsa",
+            "3 cebolas verdes",
+            "2 dentes de alho amassados",
+            "Sal a gosto",
+            "Pimenta a gosto",
+            "Pimenta-do-reino a gosto"
+        ],
+        "preparo": [
+            "1 xícara de óleo",
+            "1 xícara de vinho branco seco",
+            "2 xícaras de caldo de carne",
+            "2 cebolas em rodelas",
+            "250 g de cogumelos",
+            "1 colher de sopa de manteiga",
+            "1 colher de sopa de farinha de trigo",
+            "Pães em fatias torrados com manteiga"
+        ]
+
+
+    },
+    "modo_preparo": {
+        "preparo_lombo": [
+            "Limpe o lombo e coloque-o em uma travessa funda.",
+            "Fure toda a superfície com a ponta de um garfo."
+        ],
+        "marinada": [
+            "Prepare a marinada.",
+            "Despeje a marinada sobre o lombo, esfregando bem os temperos.",
+            "Cubra com um pano ou filme plástico e deixe marinar por 12 horas."
+        ],
+        "assamento": [
+            "Retire o lombo da marinada.",
+            "Coloque-o em forno preaquecido, regado com óleo, e asse por aproximadamente 1h30.",
+            "Durante o cozimento, regue o lombo com a mistura de óleo e caldo de carne."
+        ],
+        "molho": [
+            "Asse ou salteie os cogumelos na manteiga, temperando com sal.",
+            "Leve a marinada ao fogo e deixe ferver por alguns minutos para reduzir levemente. Reserve.",
+            "Quando o lombo estiver cozido, retire parte da gordura da assadeira.",
+            "Na gordura restante, dissolva a farinha de trigo.",
+            "Acrescente os cogumelos (e a cenoura, se utilizada) e leve ao fogo por cerca de 2 minutos."
+        ],
+        "finalizacao": [
+            "Coloque o lombo de volta na assadeira.",
+            "Cubra com o molho.",
+            "Sirva quente, decorado com as torradas."
+        ]
+    }
+
+},
+{
+    "id": 7,
+    "nome": "",
+    "categoria": "",
+    "tags": [
+        "#",
+    ],
+    "forma": "",
+    "rendimento": "",
+    "tempo_preparo": {
+        "preparo": " ",
+        "Cocção": " ",
+        "total": ""
+    },
+    "ingredientes": {
+        "massa": [
+            "",
+        ],
+        "recheio": [],
+        "calda": [],
+        "reducao": []
+    },
+    "modo_preparo": {
+        "massa": [],
+        "recheio": [],
+        "reducao": [],
+        "montagem": []
+    }
+},
+
+
+]
---
+++ src/styles/global.tsx
@@ -0,0 +1,15 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const cores = {
+    
+}
+
+export const GlobalCSs = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-shadow: border-box;
+}
+
+`
